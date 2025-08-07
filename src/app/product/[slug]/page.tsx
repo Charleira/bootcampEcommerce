57e477d8e013520c968055bd48e48a8ea30961bc
Carlos Piacente <cadupiacente67@gmail.com>
2025-08-07 10:27:32 -0300

    feat: product page not finished

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import { eq } from 'drizzle-orm'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+
+import { Header } from '@/components/common/header'
+import { db } from '@/db'
+import { productVariantTable } from '@/db/schema'
+
+interface ProductVariantPageProps {
+    params: Promise<{ slug: string }>
+}
+
+const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
+    const { slug } = await params
+    const productVariant = await db.query.productVariantTable.findFirst({
+        where: eq(productVariantTable.slug, slug),
+        with: { product: true },
+    })
+    if (!productVariant) {
+        return notFound()
+    }
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col space-y-6">
+                {/* {IMAGEM} */}
+                <div className="relative h-[380px] w-full rounded-3xl">
+                    <Image
+                        src={productVariant.imageUrl}
+                        alt={productVariant.name}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProductVariantPage
